6c7fff2450db9f056c9b6b27b12b769e106444cc
LoGinto <46216287+LoGinto@users.noreply.github.com>
2022-11-19 15:54:50 +0400

    responsive design 2/4

--- MenuForMobile.js
+++ MenuForMobile.js
@@ -1,12 +1,14 @@
 const _list = document.getElementById("NavMobileUl");
 const _menuImage = document.getElementById("MenuImage");
 const _mainBody = document.getElementById("MainBody");
+const _logo = document.getElementById("LogoImage");
 
 function OpenList(){
     console.log("Open btn");
     _list.style.display = "block";
     _menuImage.style.display = "none";
     _mainBody.style.display = "none";
+    _logo.style.display = "none";
 }
 function CloseList(){
     console.log("Close btn");
@@ -14,4 +16,5 @@ function CloseList(){
         _list.style.display = "none";
         _menuImage.style.display = "block";
         _mainBody.style.display = "block";
+        _logo.style.display = "block";
 }
